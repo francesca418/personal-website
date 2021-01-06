509c11aac5f2e0ccc3bf742ecb32175aa0b882a4
francesca418 <48101104+francesca418@users.noreply.github.com>
2021-01-06 01:10:58 -0500

    favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,7 +57,7 @@ module.exports = {
         background_color: config.backgroundColor,
         theme_color: config.themeColor,
         display: 'standalone',
-        icon: 'static/logo/favicon.png',
+        icon: config.favicon,
       },
     },
     'gatsby-plugin-offline',
